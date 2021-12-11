2a9c70ca0c8472a2c0d28467ac6692c7ce4a7a7e
tatsumakimorosuke <zigzagmorokichi.com>
2021-12-11 06:51:08 +0000

    id

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ class IndexView(View):
         profile_data = Profile.objects.all()
         if profile_data.exists():
             profile_data = profile_data.order_by('-id')[0]
-        work_data = Work.objects.order_by('id')
+        work_data = Work.objects.order_by('-id')
         return render(request, 'app/index.html',{
         'profile_data': profile_data,
         'work_data': work_data
